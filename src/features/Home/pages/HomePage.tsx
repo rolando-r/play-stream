f5c3fc5871093116e0b6f656df80d694518032ea
rolando-r <131362377+rolando-r@users.noreply.github.com>
2025-08-12 17:29:10 -0500

    feat: update Navbar an images

--- src/features/Home/pages/HomePage.tsx
+++ src/features/Home/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 export const HomePage = () => {
   return (
     <div className="text-black-500 p-4">
-      <h1 className="text-2xl font-bold">Bienvenido a FilmHub</h1>
+      {/* <h1 className="text-2xl font-bold">Bienvenido a FilmHub</h1> */}
     </div>
   );
 }
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,17 +1,6 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-    const [scrolled, setScrolled] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setScrolled(window.scrollY > 10);
-        };
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-
     return (
         <nav
             className={`
@@ -26,10 +15,6 @@ export const Navbar = () => {
                     space-x-4 rounded-full
                     transition-all duration-300 ease-in-out transform
                     text-xs sm:text-sm md:text-base
-                    ${scrolled
-                        ? "backdrop-blur-md bg-white/70 dark:bg-gray-800/70 text-zinc-800 dark:text-zinc-100 shadow-md"
-                        : "bg-zinc-100 text-zinc-800 dark:bg-gray-900 dark:text-zinc-100"
-                    }
                 `}
             >
                 {/* Logo como imagen */}
@@ -53,6 +38,14 @@ export const Navbar = () => {
                 </Link>
                 <Link to="/movies" className="hover:underline">
                     Anime
+                </Link>
+
+                <Link to="/">
+                    <img
+                        src="/search.svg"
+                        alt="search"
+                        className="max-h-5 w-auto object-contain"
+                    />
                 </Link>
             </div>
         </nav>
