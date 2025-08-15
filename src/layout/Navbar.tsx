90812c0803b04fb9202f782e4f9a89f1248f44b0
rolando-r <131362377+rolando-r@users.noreply.github.com>
2025-08-15 15:36:13 -0500

    fix: Repair responsive logos in navbar

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -39,17 +39,18 @@ export const Navbar = () => {
     >
       <div className="flex items-center justify-between py-2 w-full max-w-[95%] mx-auto">
         {/* Logo */}
-        <div className="w-20 flex justify-center items-center">
+        <div className="flex justify-center items-center">
           <Link to="/">
             <img
               src="/logo.webp"
               alt="FilmHub"
-              className="h-12 sm:h-16 object-contain"
+              className="h-10 sm:h-14 md:h-16 object-contain"
             />
           </Link>
         </div>
+
         {/* Central Menu */}
-        <div className="relative flex space-x-6 font-bold">
+        <div className="relative flex space-x-3 sm:space-x-6 font-bold text-sm sm:text-base">
           {menuItems.map(({ path, label }, index) => (
             <Link
               key={path}
@@ -76,12 +77,12 @@ export const Navbar = () => {
         </div>
 
         {/* Search Icon */}
-        <div className="w-20 flex justify-center items-center">
+        <div className="flex justify-center items-center">
           <Link to="/">
             <img
               src="/search.svg"
               alt="search"
-              className="h-6 sm:h-8 object-contain"
+              className="h-5 sm:h-6 md:h-7 object-contain"
             />
           </Link>
         </div>
